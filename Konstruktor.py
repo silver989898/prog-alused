ad02947f6ea49061a7775fb8e919e686c767d981
Silver <silver.kilusk@voco.ee>
2024-01-15 13:09:03 +0200

    Valmis

---
+++ Konstruktor.py
@@ -0,0 +1,66 @@
+"""Constructor exercise."""
+
+
+class Empty:
+    """An empty class without constructor."""
+
+    pass
+
+
+class Person:
+    def __init__(self):
+        self.firstname = ""
+        self.lastname = ""
+        self.age = 0
+    """Represent person with firstname, lastname and age."""
+    pass
+
+
+class Student:
+    def __init__(self, firstname, lastname, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+    """Represent student with firstname, lastname and age."""
+
+    pass
+
+
+if __name__ == '__main__':
+    # empty usage
+    def Empty():
+        pass
+
+    # 3 x person usage
+    def Person(self):
+        person.firstname = "Aadu"
+        person.lastname = "Kuusk"
+        person.age = "35"
+
+    def Person(self):
+        person.firstname = "Badu"
+        person.lastname = "Buusk"
+        person.age = "36"
+
+    def Person(self):
+        person.firstname = "Ladu"
+        person.lastname = "Luusk"
+        person.age = "37"
+
+    # 3 x student usage
+    def Student(self):
+        student.firstname = "Karl"
+        student.lastname = "Tamm"
+        student.age = "45"
+
+    def Student(self):
+        student.firstname = "Marl"
+        student.lastname = "Mamm"
+        student.age = "46"
+        
+    def Student(self):
+        student.firstname = "Larl"
+        student.lastname = "Lamm"
+        student.age = "47"
+
+    pass
