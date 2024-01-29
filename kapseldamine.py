228b101f27164cad95c37a323b2413420ae68619
Silver <silver.kilusk@voco.ee>
2024-01-29 13:50:37 +0200

    pooleli

---
+++ kapseldamine.py
@@ -0,0 +1,30 @@
+"""Encapsulation exercise."""
+
+
+class Student:
+    """Represent student with name, id and status."""
+    def __init__(self, name, id, status = "Active"):
+        self.__name = ""
+        self.__id = 0
+        self.__status = "Active"
+    
+    def Student(self):
+        student.name = "Karl"
+        student.__id = 123
+        student.status = "Active"
+
+        
+    def get_id(self):
+        student.id
+    def set_name(self, name):
+        student.name = 'Garl'
+    def set_status(self, status):
+        if student.status == 'Active' or 'Expelled' or 'Finished' or 'Inactive':
+            student.status = 'Changed'
+        else:
+            pass
+    def get_status(self):
+        student.get_status
+        
+    pass
+
