e8d23ad3e422a1d1286b56d9b745efb4b3cfa8b9
silver989898 <147377876+silver989898@users.noreply.github.com>
2024-02-28 11:41:15 +0200

    Update kapseldamine.py
    Pooleli

--- kapseldamine.py
+++ kapseldamine.py
@@ -3,32 +3,34 @@
 
 class Student:
     """Represent student with name, id and status."""
-    def __init__(self, name = str, id = float, status = str):
+    
+    def __init__(self, name = str, id = float, status = "Active"):
         self.__name = name
         self.__id = id
         self.__status = "Active"
-    
+
     def Student(self):
         Student.__name = name
         Student.__id = id
-        Student.status = "Active"
-    
+        Student.__status = "Active"
+        
     def get_id(self):
-        return Student.id
+        return self.__id
+    
     def set_name(self, name):
-        Student.name = name
+        self.__name = name
     
     def get_name(self):
-        return Student.name
+        return self.__name
     
     def set_status(self, status):
-        if Student.status == 'Active' or 'Expelled' or 'Finished' or 'Inactive':
-            Student.status = "Active"
+        if self.__status == "Active" or "Expelled" or "Finished" or "Inactive":
+            self.__status = "Active"
         else:
             pass
     
     def get_status(self):
-        Student.get_status
+        return self.__status
+    
     
     pass
-
